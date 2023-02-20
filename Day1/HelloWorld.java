2614f406c9997957ea224a909c7cdd3ab2e632dc
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-02-20 23:00:56 +0530

    Hello World Program

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,16 @@
+package Day1;
+
+/*   Created by IntelliJ IDEA.
+*   Author: Hemant Mudgal
+*   Date: 20-02-2023
+*   Time: 22:56
+*   File: Day1.HelloWorld.java
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World"); // print() is used for printing something on the console without new line
+        System.out.print("Hello World \n"); // "\n" represents the new line, or it breaks the line and start the text to the new line.
+        System.out.println("Hello World"); // println() used for printing and ended to the new line automatically.
+    }
+}
