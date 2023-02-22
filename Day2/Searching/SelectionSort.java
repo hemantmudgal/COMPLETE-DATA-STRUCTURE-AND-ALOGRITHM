0d9ea1abca247b233647687d3c8eecb7b26f1e23
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-02-22 23:20:26 +0530

    Added Selection sort class in Day 2 folder

---
+++ Day2/Searching/SelectionSort.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 22-02-2023
+ *   Time: 23:12
+ *   File: SelectionSort.java
+ */
+
+package Day2.Searching;
+
+public class SelectionSort {
+}
