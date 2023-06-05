c38e1153fd76f2a9badd40f1ce4fd1fe385f3f8f
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-06-05 23:10:28 +0530

    updated bubble sort

--- Day2/Sorting/BubbleSort.java
+++ Day2/Sorting/BubbleSort.java
@@ -18,7 +18,7 @@ public class BubbleSort {
     }
 
     private static void bubbleSort(int[] array) {
-        for(int i = 0; i < array.length; i++){
+        for(int i = 0; i < array.length - 1; i++){
             for(int j = 1 ; j < array.length - i; j++){
                 if(array[j] < array[j-1]){
                     int temp = array[j];
